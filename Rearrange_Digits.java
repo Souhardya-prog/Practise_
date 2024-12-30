3479c01cd102b4acc9c321771568169643b44e49
Souhardya <bhowalsouhardya@gmai.com>
2024-12-30 21:30:22 +0530

    Simplified arrange digits, still some bugs are present

--- Rearrange_Digits.java
+++ Rearrange_Digits.java
@@ -1,24 +1,77 @@
-public class Rearrange_Digits {
+import java.util.Scanner;
+
+public class Rearrange_Digits{
+    static int evenNum;
+    static int oddNum;
+    static boolean isNegative = false;
     public static void main(String[] args) {
-        int n = 56123;
-        int ODC=0, EDC=0;
-        int odd=0, even=0;
-        int digitsCount = (int)Math.log10(n) +1;
-        int digits = 0;
-        for (int i = 1; i <=digitsCount ; i++) {
-            digits = (n/(int)Math.pow(10, digitsCount-i))%10;
-            if(digits%2!=0){
-                odd= odd*10 + digits;
-                ODC++;
-            }if(digits%2==0){
-                even= even*10 + digits;
-                EDC++;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n = scanner.nextInt();
+        if(n < 0){
+            isNegative = true;
+            n = Math.abs(n);
+        }
+        int digitCount = getDigitCount(n);
+        for (int i = 0; i < digitCount; i++) {
+            if(getDigits(n) % 2 == 0){
+                formEvenNum(getDigits(n));
+            }
+            else{
+                formOddNum((getDigits(n)));
+            }
+            n = n%(int)Math.pow(10, getDigitCount(n) - 1);
+        }
+        showRearrangedNumber();
+    }
+    static int getDigitCount(int n){
+        return (int)Math.log10(n)+1 ;
+    }
+    static int getDigits(int n){
+        return n/(int)Math.pow(10, getDigitCount(n) - 1);
+    }
+    static void formEvenNum(int evenDigit){
+        evenNum = evenNum *10 + evenDigit;
+    }
+    static void formOddNum(int oddDigit){
+        oddNum = oddNum *10 + oddDigit;
+    }
+    static void showRearrangedNumber(){
+//        oddNum, evenNum.
+        long Rearranged;
+        if(isNegative){
+            Rearranged = (int) (evenNum * Math.pow(10, getDigitCount(oddNum)) + oddNum);
+            if(Rearranged>Integer.MAX_VALUE || Rearranged<Integer.MIN_VALUE){
+                System.out.println("Rearranged number with even digits placed first is out the integer range: ");
+            }
+            else{
+                System.out.println("Rearranged number with even digits placed first: "+ -Rearranged);
+            }
+
+            Rearranged = (int) (oddNum * Math.pow(10, getDigitCount(evenNum)) + evenNum);
+            if(Rearranged>Integer.MAX_VALUE || Rearranged<Integer.MIN_VALUE){
+                System.out.println("Rearranged number with even digits placed first is out the integer range: ");
+            }
+            else{
+                System.out.println("Rearranged number with even digits placed first: "+ -Rearranged);
             }
         }
-        System.out.println("Original number: "+n);
-        n= even*(int)Math.pow(10, ODC) + odd;
-        System.out.println("Even digits kept first: "+n);
-        n=odd*(int)Math.pow(10, EDC) + even;
-        System.out.println("Odd digits kept first: "+n);
+        else{
+            Rearranged = (int) (evenNum * Math.pow(10, getDigitCount(oddNum)) + oddNum);
+            if(Rearranged>Integer.MAX_VALUE || Rearranged<Integer.MIN_VALUE){
+                System.out.println("Rearranged number with even digits placed first is out the integer range: ");
+            }
+            else{
+                System.out.println("Rearranged number with even digits placed first: "+Rearranged);
+            }
+
+            Rearranged = (int) (oddNum * Math.pow(10, getDigitCount(evenNum)) + evenNum);
+            if(Rearranged>Integer.MAX_VALUE || Rearranged<Integer.MIN_VALUE){
+                System.out.println("Rearranged number with even digits placed first is out the integer range: ");
+            }
+            else{
+                System.out.println("Rearranged number with even digits placed first: "+Rearranged);
+            }
+        }
     }
 }
